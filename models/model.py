abada3e06c9b419da12c317f8c99bec024e7dd0d
Wellington <wellingtoncarvalho11@gmail.com>
2022-11-02 01:43:03 -0300

    feature: aplicando conceito de heranca

--- models/model.py
+++ models/model.py
@@ -1,74 +1,55 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.__ano = ano
-        self.__duracao = duracao
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self._ano = ano
+        self._likes = 0
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
 
     @property
     def ano(self):
-        return self.__ano
+        return self._ano
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome
+
+    def dar_like(self):
+        self._likes += 1
+
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self._duracao = duracao
 
     @property
     def duracao(self):
-        return self.__duracao
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome
-
-    def dar_like(self):
-        self.__likes += 1
+        return self._duracao
 
 
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.__ano = ano
-        self.__temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome
-
-    @property
-    def ano(self):
-        return self.__ano
+        super().__init__(nome, ano)
+        self._temporadas = temporadas
 
     @property
     def temporadas(self):
-        return self.__temporadas
-
-    def dar_like(self):
-        self.__likes += 1
+        return self._temporadas
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-      f'- Duração: {vingadores.duracao}'
-      f'- Likes: {vingadores.likes}')
+print(f'{vingadores.nome} - {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
 atlanta = Serie('Atlanta', 2022, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
-      f'- Temporadas: {atlanta.temporadas}'
-      f'- Likes: {atlanta.likes}')
+print(f'{atlanta.nome} - {atlanta.ano} - Duração: {atlanta.temporadas} - Likes: {atlanta.likes}')
+
